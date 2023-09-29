3dbd2ff67845b10a11810b5747657e1128afa322
Diana <diana.huang32@gmail.com>
2023-09-29 03:04:32 -1000

    cookie name

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ app.use(
     secret: process.env.SECRET_KEY,
     resave: false,
     saveUninitialized: true,
+    name: "CookieName",
     cookie: { httpOnly: false, secure: true, sameSite: "none" },
   })
 );
